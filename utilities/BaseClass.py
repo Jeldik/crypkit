afee68749865f2eb21befa99745e1d5da6f092b8
Tomas Jelinek <jeldicek@gmail.com>
2020-06-09 22:03:22 +0200

    promazani log handleru

--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -11,6 +11,8 @@ class BaseClass:
         loggerName = inspect.stack()[1][3]
         logger = logging.getLogger(loggerName)
         fileHandler = logging.FileHandler("logfile.log")
+        if logger.hasHandlers():
+            logger.handlers.clear()
         logger.addHandler(fileHandler)
         formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
         fileHandler.setFormatter(formatter)
